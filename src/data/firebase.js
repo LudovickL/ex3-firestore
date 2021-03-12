715064c0bae94a00cdd834146e7de3d8268c05f4
LudovickL <Luberisselud@gmail.com>
2021-03-11 19:25:52 -0500

    modif fichier firebase.js

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -23,5 +23,5 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
-const firestore = firebase.firestore();
+const firestore = fbAppli.firestore();
 export default firestore;
