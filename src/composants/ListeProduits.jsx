f42aa5882a08b0df413d68ca8bcda087736e1018
LudovickL <Luberisselud@gmail.com>
2021-03-11 20:14:06 -0500

    modifications

--- src/composants/ListeProduits.jsx
+++ src/composants/ListeProduits.jsx
@@ -3,13 +3,13 @@ import Produit from "./Produit";
 import { useEffect, useState } from 'react';
 /******* Ex#3 - Étape D ********************************/ 
 // Importer l'objet bd du fichier firebase.js
-import firestore from '../data/firebase';
+import bd from '../data/firebase';
 
 
-export default function ListeProduits(props) {
+export default function ListeProduits({props}) {
   /******* Ex#3 - Étape E ********************************/ 
   // Créer un "état" React pour les produits (utiliser useState)
-  const [produits] = useState([])
+  const [produits] = useState([]);
     
   useEffect(() => {
     async function getProduits() {
@@ -22,7 +22,7 @@ export default function ListeProduits(props) {
       // [Suggestion : remarquez que la fonction getProduits() est marquée 'async'. Lorsque vous appelez la méthode Firestore qui retourne les produits, cette fonction 
       // est une Promesse, vous pouvez simplement utiliser la syntax 'await' pour attendre le résultat avant de remplir le tableau tabProduits 
       // (visionnez la capsule au sujet du code asynchrone en JavaScript)]
-      const reponse = await firestore.collection('ex3-produits').get();
+      const reponse = await bd.collection('ex3-produits').get();
       
       /******* Ex#3 - Étape G ********************************/ 
       // Modifier l'état des produits (initialisé ci-dessus avec useState) en utilisant le mutateur et le tableau tabProduits
@@ -47,7 +47,9 @@ export default function ListeProduits(props) {
         */}
         {
           produits.map(
-            produit => <li key={produit.id}><Produit id={produit.id} nom={produit.nom} prix={produit.prix}/></li>
+            produit => <li key={produit.id}>
+              <Produit id={produit.id} nom={produit.nom} prix={produit.prix}/>
+              </li>
           )
         }
       </ul>
--- src/data/firebase.js
+++ src/data/firebase.js
@@ -23,5 +23,4 @@ if(!fbAppli.apps.length) {
 const bd = fbAppli.firestore();
 /******* Ex#3 - Étape C ********************************/ 
 // Exporter (par défaut) la référence à "bd" pour pouvoir l'utiliser dans le composant ListeProduits
-const firestore = fbAppli.firestore();
-export default firestore;
+export default bd;
